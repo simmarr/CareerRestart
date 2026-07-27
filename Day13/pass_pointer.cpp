f007b6fbdb4daf96a634452d31d978e7d1aac582
Simardeep Singh <simar147369@gmail.com>
2026-07-28 01:33:16 +0530

    pass by pointer

---
+++ Day13/pass_pointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void addten(int *x){
+
+    *x=*x+10;
+}
+
+int main(){
+
+    int a=5;
+
+    cout<<a<<endl;
+
+    addten(&a);
+
+    cout<<a<<endl;
+
+    return 0;
+}
